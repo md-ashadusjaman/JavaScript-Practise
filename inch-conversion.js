d0b6bcc4439d0881c22ccfbf35a99660665ece33
Md.Ashadus Jaman <mdarishjaman@gmail.com>
2021-01-21 18:55:44 +0600

    using function

---
+++ inch-conversion.js
@@ -0,0 +1,13 @@
+// create a function and use this
+
+function inchToFeet(inch) {
+    var feet = inch / 12;
+    return feet;
+}
+
+var nanaFeet = inchToFeet(284);
+console.log(nanaFeet);
+
+
+var robinFeet = inchToFeet(390);
+console.log(robinFeet);
