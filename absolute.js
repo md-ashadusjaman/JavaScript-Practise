66efcb02d9853925612e819745bf8ec655ad9e31
Md.Ashadus Jaman <mdarishjaman@gmail.com>
2021-01-21 17:03:51 +0600

    first commit

---
+++ absolute.js
@@ -0,0 +1,43 @@
+// how to convert any negetive number to positive
+
+var num = -40;
+var number = -94;
+
+var result = Math.abs(number);
+var result1 = Math.abs(num);
+
+console.log(result, result1);
+
+
+// make float number to round
+
+var num = 6.98429;
+var num1 = 4.49999;
+
+var rounded = Math.round(num);
+var rounded1 = Math.round(num1);
+
+console.log(rounded, rounded1);
+
+
+// Using of Ceil
+
+
+var num = 3.49999;
+
+var result = Math.ceil(num);
+
+console.log(result);
+
+
+// using the floor
+
+var num = 3.49999;
+var result = Math.floor(num);
+console.log(result);
+
+// using the random
+
+var num = Math.random() * 100;
+var result = Math.round(num);
+console.log(result);
